964c7eb82936880e8fa3b89cd9f152419cfd990d
radhadevi11 <radhadevi16isr033@gmail.com>
2018-09-16 19:40:51 +0530

    corrected the LatLng of MCN station

---
+++ src/com/radha/railway/service/NoSuchFromStationException.java
@@ -0,0 +1,7 @@
+package com.radha.railway.service;
+
+public class NoSuchFromStationException extends Exception {
+    public NoSuchFromStationException(String message) {
+        super(message);
+    }
+}
