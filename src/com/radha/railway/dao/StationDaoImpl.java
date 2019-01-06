f72395aa8d1882fe3cc5c4c59b93512b57e94826
radhadevi11 <radhadevi16isr033@gmail.com>
2019-01-06 21:56:52 +0530

    Implementation of get method in StationDaoImpl.java testGet method execution in StationDaoImplTest.java

--- src/com/radha/railway/dao/StationDaoImpl.java
+++ src/com/radha/railway/dao/StationDaoImpl.java
@@ -6,12 +6,14 @@ import java.sql.*;
 
 public class StationDaoImpl implements Dao<Station>{
     private PreparedStatement saveStatement;
+    private PreparedStatement getStatement;
     private Connection connection;
 
     public StationDaoImpl() {
         DbConnection dataBaseConnection = new DbConnection();
         connection = dataBaseConnection.getConnection();
         createSaveStatement();
+        createGetStatement();
     }
 
     public int save(Station station){
@@ -32,7 +34,7 @@ public class StationDaoImpl implements Dao<Station>{
             resultSet.next();// make the ResultSet object to point the next row,initially it point to row before the first row
             return resultSet.getInt(1);//getting the primary key which is in the first column
         } catch (SQLException e) {
-           throw new RuntimeException("can not save station "+station.getName(),e);
+            throw new RuntimeException("can not save station "+station.getName(),e);
         }
         finally {
             try {
@@ -48,10 +50,54 @@ public class StationDaoImpl implements Dao<Station>{
 
     private void createSaveStatement() {
         try {
-          saveStatement = connection.prepareStatement("insert into station(station_name,station_code)" +
+            saveStatement = connection.prepareStatement("insert into station(station_name,station_code)" +
                     "values(?,?)", Statement.RETURN_GENERATED_KEYS);
         } catch (SQLException e) {
             throw new RuntimeException("Can not create PreparedStatement to create station",e);
         }
+    }
+
+    public Station get(int id) {
+        /*Steps:
+        i)Declare 2 String variable called stationName and stationCode
+        ii)execute a sql query for select
+        iii)get the station name and store it in the stationName
+        iv)get the station code and store it in the stationCode
+        iv)return a new Station object with the argument of stationCode and stationName
+
+         */
+
+        try {
+
+
+            getStatement.setInt(1, id);//set the id for the select query here 1 is the parameterIndex
+            ResultSet resultSet = getStatement.executeQuery();
+            if(!resultSet.next()){
+                return null;
+            }
+            String stationName = resultSet.getString("station_name");
+            String stationCode = resultSet.getString("station_code");
+            return new Station(stationCode, stationName);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("can not get station with id of " + id, e);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
+    private  void createGetStatement(){
+        try {
+            getStatement = connection.prepareStatement("select * from station where station_id=?");
+        } catch (SQLException e) {
+            throw new RuntimeException("Can not create PreparedStatement to get station",e);
+        }
+
     }
 }
--- test/com/radha/railway/dao/StationDaoImplTest.java
+++ test/com/radha/railway/dao/StationDaoImplTest.java
@@ -17,5 +17,27 @@ class StationDaoImplTest {
 
 
     }
+    @Test
+    public void testGetForCorrectStation(){
+        Station expected = new Station("YP","ERRUPALEM");
+
+        StationDaoImpl stationDao = new StationDaoImpl();
+
+        Station actual = stationDao.get(465);
+
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testGetForWronglStation(){
+
+        StationDaoImpl stationDao = new StationDaoImpl();
+
+        Station actual = stationDao.get(999);
+
+        assertNull(actual);
+
+    }
 
 }
